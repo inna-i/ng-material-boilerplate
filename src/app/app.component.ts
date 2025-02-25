eb30810a7ffeeef2197e77d177903e66539e3883
inna-i <iivashchuk@talend.com>
2025-02-25 09:46:25 +0100

    feat: main app structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet,HeaderComponent],
+  template: `
+    <app-header />
+    <router-outlet />
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
---
+++ src/app/model/todo.type.ts
@@ -0,0 +1,6 @@
+export type Todo = {
+    id: string;
+    userId: number;
+    title: string;
+    completed: boolean;
+}
